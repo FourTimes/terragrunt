c08bbe962d8dfa989f4870bcc6b4bbd581e297f4
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-10-19 23:56:11 +0530

    Delete module/instance directory

--- module/instance/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "vmname" {}
-variable "key_name" {}
-variable "subnet_id" {}
-variable "ami" {}
-variable "instance_type" {}
-variable "vpc_id" {}
-variable "additional_tags" {}
-variable "ports" {}
